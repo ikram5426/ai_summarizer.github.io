4be9f50bd167a523235d1373aa3c3486c38c33c1
IkramKhan <ikram5426@gmail.com>
2023-04-22 22:15:59 +0500

    Form having input for URL UI complited

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,9 +1,19 @@
+import { useEffect, useState } from "react";
 
+import { copy, linkIcon, loader, tick } from "../assets";
 
 const Demo = () => {
   return (
-    <div>Demo</div>
-  )
-}
+    <section className='w-full mt-16 max-w-xl'>
+      <div className='flex flex-col w-full gap-3'>
+        <form className="flex justify-center items-center relative" onSubmit={()=>{}}>
+          <img src={linkIcon} alt="link icon" className="w-5 my-3 ml-3 left-0 absolute" />
+          <input type="url" value={''} required  placeholder="Enter URL here" onChange={() => {} } className="url_input peer"/>
+          <button type="submit" className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700">↩</button>
+        </form>
+      </div>
+    </section>
+  );
+};
 
-export default Demo
+export default Demo;
